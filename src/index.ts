031c648f63d60fdff0a5080a4dc3e88bdb3fb315
Abdallah-Azme <abdallahazme199794@gmail.com>
2024-07-17 00:27:39 +0300

    add dep

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import express from "express";
 import { config } from "dotenv";
-
+import cors from "cors";
+import { db } from "./utils/db";
 config();
 
 const app = express();
 
 //middlewares
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 
 app.use("/api/v1", (req, res, next) => {
+  console.log(req.body);
   res.json({ message: "server is up and running", data: [] });
 });
 
