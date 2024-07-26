988c94cd2137a0c57240939cfb2306d048e51583
Abdallah-Azme <abdallahazme199794@gmail.com>
2024-07-26 17:42:53 +0300

    changes for deployments

--- api/index.ts
+++ api/index.ts
@@ -3,15 +3,16 @@ import cors from "cors";
 import { v2 as cloudinary } from "cloudinary";
 import { config } from "dotenv";
 import express from "express";
-import { catchErrorHandler } from "./middleware/catch-error-handler";
-import { deserializeUser } from "./middleware/deserialize-user";
-import { notFoundHandler } from "./middleware/not-found-route";
-import { petRoutes } from "./routes/pet.route";
-import { userRoutes } from "./routes/user.route";
 import bodyParser from "body-parser";
+import { deserializeUser } from "./middleware/deserialize-user";
+import { userRoutes } from "./routes/user.route";
+import { petRoutes } from "./routes/pet.route";
 import { gearRoutes } from "./routes/gear.route";
 import { garmentRoutes } from "./routes/garment.route";
 import { itemRoutes } from "./routes/item.route";
+import { notFoundHandler } from "./middleware/not-found-route";
+import { catchErrorHandler } from "./middleware/catch-error-handler";
+
 config();
 
 const app = express();
@@ -34,6 +35,10 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(deserializeUser);
 
+// test endpoint
+app.get("/health", (req, res, next) => {
+  return res.send({ message: "Server is up and running" });
+});
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/pets", petRoutes);
 app.use("/api/v1/gears", gearRoutes);
--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,47 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cors_1 = __importDefault(require("cors"));
+const cloudinary_1 = require("cloudinary");
+const dotenv_1 = require("dotenv");
 const express_1 = __importDefault(require("express"));
+const catch_error_handler_1 = require("./middleware/catch-error-handler");
+const deserialize_user_1 = require("./middleware/deserialize-user");
+const not_found_route_1 = require("./middleware/not-found-route");
+const pet_route_1 = require("./routes/pet.route");
+const user_route_1 = require("./routes/user.route");
+const body_parser_1 = __importDefault(require("body-parser"));
+const gear_route_1 = require("./routes/gear.route");
+const garment_route_1 = require("./routes/garment.route");
+const item_route_1 = require("./routes/item.route");
+(0, dotenv_1.config)();
 const app = (0, express_1.default)();
-app.listen(3000, () => {
-    console.log("listening on port 3000");
+cloudinary_1.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+//middlewares
+app.use((0, cors_1.default)({
+    credentials: true,
+    origin: "http://localhost:3000",
+}));
+app.use((0, cookie_parser_1.default)());
+app.use(express_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use(deserialize_user_1.deserializeUser);
+// test endpoint
+app.get("/health", (req, res, next) => {
+    return res.send({ message: "Server is up and running" });
+});
+app.use("/api/v1/users", user_route_1.userRoutes);
+app.use("/api/v1/pets", pet_route_1.petRoutes);
+app.use("/api/v1/gears", gear_route_1.gearRoutes);
+app.use("/api/v1/garments", garment_route_1.garmentRoutes);
+app.use("/api/v1/items", item_route_1.itemRoutes);
+app.all("*", not_found_route_1.notFoundHandler);
+app.use(catch_error_handler_1.catchErrorHandler);
+app.listen(process.env.PORT, () => {
+    console.log("listening on port " + process.env.PORT);
 });
